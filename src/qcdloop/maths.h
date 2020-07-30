fb9d7337c105018071528cf95ceefe987ca5aae9
Stefano Carrazza <stefano.carrazza@cern.ch>
2020-07-30 15:27:59 +0200

    replacing fabs with abs

--- src/qcdloop/maths.h
+++ src/qcdloop/maths.h
@@ -36,7 +36,7 @@ namespace ql
   // Absolute value
   inline double  Abs(double const& x)   { return std::fabs(x); }
   inline qdouble Abs(qdouble const& x)  { return fabsq(x);}
-  inline double  Abs(complex const& x)  { return std::fabs(x);}
+  inline double  Abs(complex const& x)  { return std::abs(x);}
   inline qdouble Abs(qcomplex const& x) { return cabsq(x); }
 
   // Complex tools, imag, real and conj.
