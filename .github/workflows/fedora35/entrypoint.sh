36b78d46caf5dae197f129e0152484b449cb7597
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2022-08-18 17:28:19 +0200

    Cleanup

--- .github/workflows/fedora35/entrypoint.sh
+++ .github/workflows/fedora35/entrypoint.sh
@@ -2,9 +2,7 @@
 set -x
 uname -a 
 cat /etc/issue
-yum -y install  epel-release dnf-plugins-core
-dnf config-manager --set-enabled powertools
-yum -y install  gcc gcc-c++ gcc-gfortran make which cmake cmake-data cmake-filesystem 
+dnf -y install  gcc gcc-c++ gcc-gfortran make which cmake cmake-data cmake-filesystem 
 
 cmake -S . -B BUILD -DCMAKE_INSTALL_PREFIX=$(pwd)/INSTALL
 cmake --build BUILD
